eebea5d123ed7fabee38a270cfd0fcc5f87e99f6
Neerajreddy200605 <ponnaneerajreddy@gmail.com>
2025-10-31 22:43:22 +0530

    final commit

--- client/src/pages/Cart.jsx
+++ client/src/pages/Cart.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { useAppContext } from "../context/AppContext";
+import { useAppContext } from "../context/AppContext.jsx";
 import { assets, dummyAddress } from "../assets/assets";
 import toast from "react-hot-toast";
 
--- client/src/pages/MyOrders.jsx
+++ client/src/pages/MyOrders.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { useAppContext } from "../context/AppContext";
+import { useAppContext } from "../context/AppContext.jsx";
 
 const MyOrders = () => {
   const [myOrders, setMyOrders] = useState([]);
